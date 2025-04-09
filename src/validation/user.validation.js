a0a308a5e00eb8e16667a6260040cde590a83142
Akbar Ramadhan <akbarrmdhan175@gmail.com>
2025-04-09 22:35:50 +0700

    edit fix(validation): add asterisks to error messages for consistency

--- src/validation/user.validation.js
+++ src/validation/user.validation.js
@@ -3,29 +3,29 @@ const { body } = require('express-validator');
 const validateRegister = [
     body('name')
         // .isString().withMessage('Nama harus berupa teks')
-        .isLength({ min: 3, max: 50 }).withMessage('Nama harus antara 3-50 karakter'),
+        .isLength({ min: 3, max: 50 }).withMessage('*Nama harus antara 3-50 karakter'),
 
     body('email')
-        .isEmail().withMessage('Format email tidak valid'),
+        .isEmail().withMessage('*Format email tidak valid'),
 
     body('password')
-        .isLength({ min: 6 }).withMessage('Password minimal 6 karakter'),
+        .isLength({ min: 6 }).withMessage('*Password minimal 6 karakter'),
 
     // body('image')
     //     .optional(),
         // .isURL().withMessage('Gambar harus berupa URL valid'),
 
     body('phone_number')
-        .isNumeric().withMessage('Nomor telepon harus berupa angka')
-        .isLength({ min: 10, max: 15 }).withMessage('Nomor telepon harus lebih 10 digit'),
+        .isNumeric().withMessage('*Nomor telepon harus berupa angka')
+        .isLength({ min: 10, max: 15 }).withMessage('*Nomor telepon harus lebih 10 digit'),
 ];
 
 const validateLogin = [
     body('identifier')
-        .notEmpty().withMessage('Masukkan email atau nomor telepon '),
+        .notEmpty().withMessage('*Masukkan email atau nomor telepon '),
 
     body('password')
-        .isLength({ min: 6 }).withMessage('Password minimal 6 karakter')
+        .isLength({ min: 6 }).withMessage('*Password minimal 6 karakter')
 ];
 
 
