d663fa2529b984630f38d3e0c957556e07c278ad
Akbar Ramadhan <akbarrmdhan175@gmail.com>
2025-04-13 21:22:05 +0700

    edit(app): update server log message format for clarity

--- app.js
+++ app.js
@@ -38,5 +38,5 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/partner", partnerRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on... http://localhost:${port}`);
 });
