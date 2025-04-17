331f023794ab068deee31e5ee9e4f64a473d21d6
Akbar Ramadhan <akbarrmdhan175@gmail.com>
2025-04-18 02:26:09 +0700

    refactor: comment out unused Facebook strategy implementation in facebook-config.js

--- src/auth/facebook-config.js
+++ src/auth/facebook-config.js
@@ -21,23 +21,23 @@ passport.use('facebook-token',new FacebookTokenStrategy({
 }));
 
 
-passport.use('facebook-link', new FacebookStrategy({
-    clientID: process.env.FB_CLIENT_ID,
-    clientSecret: process.env.FB_CLIENT_SECRET,
-    callbackURL: process.env.FB_CALLBACK_URL,
-    profileFields: ['id', 'displayName', 'photos', 'email'],
-    enableProof: false
-}, async (accessToken, refreshToken, profile, done) => {
-    try {
-        console.log('FB Profile:', profile);
-        profile.accessToken = accessToken;
+// passport.use('facebook-link', new FacebookStrategy({
+//     clientID: process.env.FB_CLIENT_ID,
+//     clientSecret: process.env.FB_CLIENT_SECRET,
+//     callbackURL: process.env.FB_CALLBACK_URL,
+//     profileFields: ['id', 'displayName', 'photos', 'email'],
+//     enableProof: false
+// }, async (accessToken, refreshToken, profile, done) => {
+//     try {
+//         console.log('FB Profile:', profile);
+//         profile.accessToken = accessToken;
 
-        // Di sini hanya kirim ke route, data disimpan di sana
-        return done(null, profile);
-    } catch (error) {
-        done(error, null);
-    }
-}));
+//         // Di sini hanya kirim ke route, data disimpan di sana
+//         return done(null, profile);
+//     } catch (error) {
+//         done(error, null);
+//     }
+// }));
 
 
 passport.serializeUser((user, done) => {
